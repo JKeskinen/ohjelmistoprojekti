c5b66309bf71390f781e51f60173d70485e8a64a
Juho <juho.keskinen@seamk.fi.007@gmail.com>
2026-02-10 23:00:40 +0200

    offset ampumiselle

--- PLAYER_LUOKAT/Player.py
+++ PLAYER_LUOKAT/Player.py
@@ -12,7 +12,7 @@ class Player(pygame.sprite.Sprite):
         self.input = PlayerInput()
         self.animation = PlayerAnimation(scale_factor)
         self.weapons = PlayerWeapons(scale_factor)
-        self.attack_offset_distance = 3
+        self.attack_offset_distance = 28
 
         liike_frames = frames if frames else [pygame.Surface((32, 32), pygame.SRCALPHA)]
         self.animaatio = {
